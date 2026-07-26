b6d2a8ec636a0b1ab939a8f59c5b4283a67b1c1c
@pudson <pichet.pudson@nectec.or.th>
2026-07-26 16:20:48 +0700

    Update

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+//How To Deploy A React Vite App To Github Pages (Simple)
+//https://www.youtube.com/watch?v=hn1IkJk24ow
+
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
