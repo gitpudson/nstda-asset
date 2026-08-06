919c2384d5f09955774393f517ffef6b9adbcf12
@pudson <pichet.pudson@nectec.or.th>
2026-08-06 19:43:56 +0700

    Update

--- src/components/AssetRequestMobile/AssetRequestMobile.jsx
+++ src/components/AssetRequestMobile/AssetRequestMobile.jsx
@@ -576,7 +576,7 @@ export default function AssetRequestMobile({ qrcode }) {
           <Card className="form-card">
 
             <Typography className="label">
-              รหัสครุภัณฑ์xxxx
+              รหัสครุภัณฑ์xxxxx
             </Typography>
 
             <TextField
